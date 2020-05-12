e3ec7f95f228709bbb96731fddf789ed4a461b31
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-05-12 17:53:56 +0100

    keypress event

--- js/script.js
+++ js/script.js
@@ -7,7 +7,28 @@ inputButton.addEventListener('click', function(){
     // console.log(event);
     if(input.value.length !==0){
         let listItem = document.createElement('li');
-        listItem.appendChild(document.createTextNode(input.value));
+        listItem.textContent = input.value;
+        unorderedList.appendChild(listItem);
+        listItem.classList.add('li');
+        let closeButton = document.createElement('button');
+        closeButton.appendChild(document.createTextNode('X'));
+        listItem.appendChild(closeButton);
+        closeButton.classList.add('closebutton');
+        input.value="";
+
+        // console.log(closeButton);
+        closeButton.addEventListener('click', function(){
+            // console.log(event);
+            unorderedList.removeChild(listItem);
+
+        })
+    }
+})
+
+input.addEventListener('keypress', function(event){
+    if(input.value.length !==0 && event.keyCode === 13){
+        let listItem = document.createElement('li');
+        listItem.textContent = input.value;
         unorderedList.appendChild(listItem);
         listItem.classList.add('li');
         let closeButton = document.createElement('button');
