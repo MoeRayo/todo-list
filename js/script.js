f4153f9d8f36b8e7e862f500a3d2823d633993bb
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-05-12 17:38:37 +0100

    delete button working

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ let input = document.getElementById('input');
 let inputButton = document.getElementById('inputbutton');
 let unorderedList = document.querySelector('ul');
 
-// function createElememt(){
-
-// }
 inputButton.addEventListener('click', function(){
+    // console.log(event);
     if(input.value.length !==0){
         let listItem = document.createElement('li');
         listItem.appendChild(document.createTextNode(input.value));
@@ -16,9 +14,13 @@ inputButton.addEventListener('click', function(){
         closeButton.appendChild(document.createTextNode('X'));
         listItem.appendChild(closeButton);
         closeButton.classList.add('closebutton');
-        // closeButton.classList.add('closebutton');
-    
         input.value="";
+
+        // console.log(closeButton);
+        closeButton.addEventListener('click', function(){
+            // console.log(event);
+            unorderedList.removeChild(listItem);
+
+        })
     }
-   
 })
