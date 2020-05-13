262cf20190194199316fc65ce83d41e2a0df141c
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-05-13 03:39:53 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -3,45 +3,39 @@ let input = document.getElementById('input');
 let inputButton = document.getElementById('inputbutton');
 let unorderedList = document.querySelector('ul');
 
-inputButton.addEventListener('click', function(){
+function createListItems(){
+    let listItem = document.createElement('li');
+    listItem.textContent = input.value;
+    unorderedList.appendChild(listItem);
+    listItem.classList.add('li');
+    let closeButton = document.createElement('button');
+    closeButton.appendChild(document.createTextNode('X'));
+    listItem.appendChild(closeButton);
+    closeButton.classList.add('closebutton');
+    input.value="";
+    
+    // the "X" value on the button 
+    closeButton.addEventListener('click', function(){
+        // console.log(closeButton);
+        unorderedList.removeChild(listItem);
+    })
+}
+
+function listOnClick(){
     // console.log(event);
     if(input.value.length !==0){
-        let listItem = document.createElement('li');
-        listItem.textContent = input.value;
-        unorderedList.appendChild(listItem);
-        listItem.classList.add('li');
-        let closeButton = document.createElement('button');
-        closeButton.appendChild(document.createTextNode('X'));
-        listItem.appendChild(closeButton);
-        closeButton.classList.add('closebutton');
-        input.value="";
-
-        // console.log(closeButton);
-        closeButton.addEventListener('click', function(){
-            // console.log(event);
-            unorderedList.removeChild(listItem);
-
-        })
+       createListItems();
     }
-})
+}
 
-input.addEventListener('keypress', function(event){
+// for the keyboard enter keypress event
+function listOnEnter(event){
     if(input.value.length !==0 && event.keyCode === 13){
-        let listItem = document.createElement('li');
-        listItem.textContent = input.value;
-        unorderedList.appendChild(listItem);
-        listItem.classList.add('li');
-        let closeButton = document.createElement('button');
-        closeButton.appendChild(document.createTextNode('X'));
-        listItem.appendChild(closeButton);
-        closeButton.classList.add('closebutton');
-        input.value="";
-
-        // console.log(closeButton);
-        closeButton.addEventListener('click', function(){
-            // console.log(event);
-            unorderedList.removeChild(listItem);
-
-        })
+        createListItems();
+        // console.log(event);
     }
-})
+}
+
+// the event listener calls the functions
+inputButton.addEventListener('click', listOnClick);
+input.addEventListener('keypress', listOnEnter);
